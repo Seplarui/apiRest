60d619a933f722ef3bb9195e3b3face59b750b66
Sebas Sebas <seplarui@gmail.com>
2018-11-30 21:47:41 +0100

    Creación de rutas

--- index.js
+++ index.js
@@ -10,9 +10,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-app.get('/hola/:name', (req,res)=>{
-    res.send({message: `Hola ${req.params.name}`});
+app.get('/api/product', (req, res) => {
+
+    res.send(200, { products: [] });
+
+});
+
+app.get('/api/product/:productId', (req, res) => {
+
+
+});
+
+app.post('/api/product', (req, res) => {
+    console.log(req.body);
+    res.status(404).send({message: 'El producto no existe'});
+});
+
+app.put('/api/product/:productId', (req, res) => {
+
 })
+
+app.delete('/api/product/:productId', (req, res) => {
+
+});
 
 app.listen(port, () => {
     console.log(`ÀPI REST corriendo en http://localhost:${port}`);
